ef2efb7b1750fec81764437f5d05b17a734ea187
Wesley <wes.lol25@gmail.com>
2024-07-11 14:32:54 -0300

    Patient controller

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import ip from "ip";
 import cors from "cors";
 import { Code } from "./enum/code.enum";
@@ -37,8 +37,8 @@ export class App {
     private routes(): void {
 
         this.app.use('/patients', patientRoutes);
-        this.app.get('/', (req, res) => res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, 'Welcome to the API')));
-        this.app.all('*', (req, res) => res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, this.ROUTE_NOT_FOUND)));
+        this.app.get('/', (req: Request, res: Response) => res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, 'Welcome to the API')));
+        this.app.all('*', (req: Request, res: Response) => res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, this.ROUTE_NOT_FOUND)));
 
     }
 }
